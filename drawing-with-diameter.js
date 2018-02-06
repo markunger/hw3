0775d51e0c3975a6cbbba0400d86f9c2b0194148
Mark Unger <32203932+markunger@users.noreply.github.com>
2018-02-05 21:42:10 -0800

    Create drawing-with-diameter.js

---
+++ drawing-with-diameter.js
@@ -0,0 +1,33 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(240, 20, 10);
+  } else if (key == 'G') {
+    fill(3, 200, 3);
+  } else if (key == 'B') {
+    fill(14, 24, 220); 
+  }else if (key == 'T') {
+    fill(0, 128, 128); 
+  }else if (key == 'O') {
+    fill(255, 165, 0); 
+  }else if (key == 'P') {
+    fill(128, 0, 128); 
+  }else if (key == '1') {
+    diameter = 20; 
+  }else if (key == '2') {
+    diameter = 40; 
+  }else if (key == '3') {
+    diameter = 60; 
+  }
